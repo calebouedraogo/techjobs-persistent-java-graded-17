f8db6100c20ec950686458a83c43703b0200c196
CALEB OUEDRAOGO <calebouedraogo@calebs-mbp.mynetworksettings.com>
2024-11-12 17:40:54 -0500

    Set up One-toMany Relationship

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,14 +1,16 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import jakarta.validation.Valid;
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-
-
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -16,6 +18,15 @@ import java.util.Optional;
  */
 @Controller
 public class HomeController {
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
 
     @RequestMapping("/")
     public String index(Model model) {
@@ -27,8 +38,9 @@ public class HomeController {
 
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
-	model.addAttribute("title", "Add Job");
-        model.addAttribute(new Job());
+        model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
+        model.addAttribute("job", new Job());
         return "add";
     }
 
@@ -37,17 +49,19 @@ public class HomeController {
                                        Errors errors, Model model, @RequestParam int employerId) {
 
         if (errors.hasErrors()) {
-	    model.addAttribute("title", "Add Job");
+            model.addAttribute("employers", employerRepository.findAll());
             return "add";
         }
 
+        Optional<Employer> result = employerRepository.findById(employerId);
+        if (result.isPresent()) {
+            Employer employer = result.get();
+            newJob.setEmployer(employer);
+        }
+
+        jobRepository.save(newJob);
         return "redirect:";
     }
 
-    @GetMapping("view/{jobId}")
-    public String displayViewJob(Model model, @PathVariable int jobId) {
-
-            return "view";
-    }
 
 }
