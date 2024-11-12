97c15fb639905c3769b23303b5db47fa53385f23
CALEB OUEDRAOGO <calebouedraogo@calebs-mbp.mynetworksettings.com>
2024-11-12 09:19:51 -0500

    Completed Task2:Persisting Employers and Skills

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,20 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Skill extends AbstractEntity {
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Size;
 
+@Entity
+public class Skill extends AbstractEntity {
+    @Size(max = 500, message = "Description must be under 500 characters")
+    private String description;
+
+    public Skill() {}
+
+    public @Size(max = 500, message = "Description must be under 500 characters") String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@Size(max = 500, message = "Description must be under 500 characters") String description) {
+        this.description = description;
+    }
 }
